2bdd884249b5a532a5558e9f2f2cfdf65f1f024c
Peter Perlepes <p.perlepes@gmail.com>
2022-01-25 15:36:36 +0200

    fix(chore): Fix for node-sdk-api

--- server/auth/Clerk.ts
+++ server/auth/Clerk.ts
@@ -1,9 +1,8 @@
-import { Clerk } from "@clerk/nextjs/api";
+import { users } from "@clerk/nextjs/api";
 
-export const ClerkInstance = new Clerk({ apiKey: process.env.CLERK_API_KEY });
 
 export async function getClerkUserPrimaryEmail(userId: string) {
-  const user = await ClerkInstance.users.getUser(userId);
+  const user = await users.getUser(userId);
   const primaryEmailAddress = user.emailAddresses.find(
     (emailAddress) => emailAddress.id === user.primaryEmailAddressId
   )?.emailAddress as string;
